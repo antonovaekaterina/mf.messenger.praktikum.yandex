103cefe17763327bdd045b350182a064f95ad48e
antonovaekaterina <antonova.ekaterina19@yandex.ru>
2020-11-15 01:39:14 +0700

    delete data folder

--- static/index.ts
+++ static/index.ts
@@ -5,7 +5,6 @@ import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.js";
 import ErrorPage from "./pages/ErrorPage/ErrorPage.js";
 import ChatPage from "./pages/ChatPage/ChatPage.js";
 import ProfilePage from "./pages/ProfilePage/ProfilePage.js";
-import chatProps from './data/chat.js';
 import Store from "./components/Store/Store.js";
 import reducer from './reducers/index.js';
 import {authServiceInstance} from "./services/authService.js";
@@ -21,7 +20,7 @@ export const ROUTE_NOT_FOUND = '/not-found/';
 export const ROUTE_ERROR = '/not-found/';
 
 router
-    .use(ROOT, ChatPage, chatProps)
+    .use(ROOT, ChatPage)
     .use(ROUTE_PROFILE, ProfilePage)
     .use(ROUTE_LOGIN, LoginPage)
     .use(ROUTE_REGISTRATION, RegistrationPage)
